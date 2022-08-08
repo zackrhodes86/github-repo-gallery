9f892c163049aadbe4d3989d79f8a7253e0c5bac
Zack Rhodes <zackrhodes86@gmail.com>
2022-08-08 16:25:47 -0700

    Added list of repos to the UI

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const overview = document.querySelector('.overview');
 const username = 'zackrhodes86';
+const listDisplay = document.querySelector('.repo-list');
+
+console.log(listDisplay);
 
 console.log(overview);
 console.log(username);
@@ -7,7 +10,7 @@ console.log(username);
 const getData = async function(){
   const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
-    console.log(data);
+//    console.log(data);
     displayUserData(data);
 }
 
@@ -22,7 +25,27 @@ const displayUserData = function (data) {
       <p><strong>Location:</strong> ${data.location}</p>
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div> `
-console.log(div.innerHTML);
+//console.log(div.innerHTML);
 overview.append(div);
+getRepos();
 }
+
+const getRepos = async function() {
+  const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const data = await response.json();
+  console.log(data);
+  displayRepoInfo(data);
+}
+
+const displayRepoInfo = function (repos) {
+  for (let repo of repos) {
+    let li = document.createElement('li');
+    li.classList.add('repo');
+    li.innerHTML = `<h3>${repo.name}</h3>`
+    listDisplay.append(li);
+  }
+}
+
+
+
 getData();
